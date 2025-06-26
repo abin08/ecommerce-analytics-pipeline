ba11472ff44b060d64155c9e100847aa28809863
abantony <abin.antony@oracle.com>
2025-06-26 15:56:50 +0530

    created pyspark aggregation

--- scripts/process_pipeline.py
+++ scripts/process_pipeline.py
@@ -1,5 +1,5 @@
 from pyspark.sql import SparkSession
-
+from pyspark.sql.functions import count, max as spark_max
 
 # initialize Spark
 spark = SparkSession.builder.appName("Retailkart").getOrCreate()
@@ -9,5 +9,13 @@ spark.conf.set("spark.sql.execution.arrow.pyspark.enabled", "true")
 # Load cleaned transaction data
 input_path = "data/retail_cleaned.parquet"
 df = spark.read.parquet(input_path)
+# df.show(5)
 
+# Aggregate: Purchase count & last purchase timestamp 
+# to find the purchase_count (number of times a customer bought a product) and the last_purchase_date
+agg_df = df.groupBy("CustomerID", "StockCode").agg(
+    count("*").alias("purchase_count"),
+    spark_max("InvoiceDate").alias("last_purchase_date")
+)
 
+# agg_df.show(5)
