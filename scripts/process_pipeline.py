4e91382934a8da5d9517faa4ddc5ac94015c8544
abantony <abin.antony@oracle.com>
2025-06-26 16:05:25 +0530

    feature engineering using pandas

--- scripts/process_pipeline.py
+++ scripts/process_pipeline.py
@@ -1,5 +1,6 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import count, max as spark_max
+import pandas as pd
 
 # initialize Spark
 spark = SparkSession.builder.appName("Retailkart").getOrCreate()
@@ -19,3 +20,18 @@ agg_df = df.groupBy("CustomerID", "StockCode").agg(
 )
 
 # agg_df.show(5)
+
+# Convert to Pandas via Arrow
+agg_pd = agg_df.toPandas()
+
+# Perform Feature engineering in Pandas
+# Ensure the 'last_purchase' column is in datetime format.
+agg_pd["last_purchase_date"] = pd.to_datetime(agg_pd["last_purchase_date"])
+
+# Calculate the number of days since the last purchase for each customer-product pair.
+# Subtract the last_purchase date from the current timestamp to get a timedelta,
+# then extract the number of days as an integer using .dt.days.
+agg_pd["recency_days"] = (pd.Timestamp.now() - agg_pd["last_purchase_date"]).dt.days
+print(agg_pd.head())
+
+
